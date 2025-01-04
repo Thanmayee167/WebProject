17c302bb3b5b197769f7e435476fa135052fd2b6
Thanmayee167 <thanmayee167@gmail.com>
2025-01-04 17:05:38 +0530

    calling one servlet from another servlet using URL rewriting

--- src/main/java/com/thanmayee/AddServlet.java
+++ src/main/java/com/thanmayee/AddServlet.java
@@ -2,7 +2,6 @@ package com.thanmayee;
 
 import java.io.IOException;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,12 +10,12 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet {
 	private static final long serialVersionUID = -5544861121298990712L;
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
 		int i= Integer.parseInt(req.getParameter("num1"));
 		int j= Integer.parseInt(req.getParameter("num2"));
-		req.setAttribute("result", (i+j));
-		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/square");
-		requestDispatcher.forward(req, res);
+		
+		res.sendRedirect("square?num="+ (i+j));
 	}
 
 }
--- src/main/java/com/thanmayee/SquareServlet.java
+++ src/main/java/com/thanmayee/SquareServlet.java
@@ -10,11 +10,12 @@ import jakarta.servlet.http.HttpServletResponse;
 public class SquareServlet extends HttpServlet {
 	private static final long serialVersionUID = -5544861121298990712L;
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		Object attribute = req.getAttribute("result");
-		int num = (int) attribute;
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int result = Integer.parseInt(req.getParameter("num"));
+		
 		PrintWriter writer = res.getWriter();
-		writer.println("Result is: "+(Math.pow(num, 2)));
+		writer.println("Result is: "+(Math.pow(result, 2)));
 	}
 
 }
