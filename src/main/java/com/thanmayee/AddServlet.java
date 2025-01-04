999987e35758c3aacd081041f0efea1806610846
Thanmayee167 <thanmayee167@gmail.com>
2025-01-04 11:54:02 +0530

    using doPost() method

--- src/main/java/com/thanmayee/AddServlet.java
+++ src/main/java/com/thanmayee/AddServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet {
 	private static final long serialVersionUID = -5544861121298990712L;
 
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i= Integer.parseInt(req.getParameter("num1"));
 		int j= Integer.parseInt(req.getParameter("num2"));
 		PrintWriter writer = res.getWriter();
