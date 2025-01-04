7659433683a4eff796f46c53fdb7e2d0624174e6
Thanmayee167 <thanmayee167@gmail.com>
2025-01-04 18:06:02 +0530

    called one sevlet from another servlet using cookie

--- src/main/java/com/thanmayee/AddServlet.java
+++ src/main/java/com/thanmayee/AddServlet.java
@@ -3,22 +3,23 @@ package com.thanmayee;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	private static final long serialVersionUID = -5544861121298990712L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-		
-		int i= Integer.parseInt(req.getParameter("num1"));
-		int j= Integer.parseInt(req.getParameter("num2"));
-		
-		HttpSession httpSession = req.getSession();
-		httpSession.setAttribute("num", (i+j));
-		
+
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int sum = i + j;
+
+		Cookie cookie = new Cookie("num", String.valueOf(sum));
+		res.addCookie(cookie);
+
 		res.sendRedirect("square");
 	}
 
--- src/main/java/com/thanmayee/SquareServlet.java
+++ src/main/java/com/thanmayee/SquareServlet.java
@@ -3,21 +3,25 @@ package com.thanmayee;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 public class SquareServlet extends HttpServlet {
 	private static final long serialVersionUID = -5544861121298990712L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		
-		HttpSession httpSession = req.getSession();
-		int result = (int)(httpSession.getAttribute("num"));
-		
+		int result = 0;
+
+		Cookie[] cookies = req.getCookies();
+		for (Cookie c : cookies) {
+			if (c.getName().equals("num"))
+				result = Integer.parseInt(c.getValue());
+		}
+
 		PrintWriter writer = res.getWriter();
-		writer.println("Result is: "+(Math.pow(result, 2)));
+		writer.println("Result is: " + (Math.pow(result, 2)));
 	}
 
 }
